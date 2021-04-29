af5e7a2b53cf7f6ae7a96abb79d5d2938f1b11a3
Frederico Lacis <fredlacispro@live.com>
2021-04-28 22:06:55 -0300

    Initial Commit

---
+++ VisionExperiments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisionExperiments
+//
+//  Created by Frederico Lacis de Carvalho on 28/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
